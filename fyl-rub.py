5060fbf49c49935d49fbb73f5e8cddc5cc6539d2
alone-vandal <165705034+alone-vandal@users.noreply.github.com>
2024-06-12 16:58:04 +0330

    Add files via upload

---
+++ fyl-rub.py
@@ -0,0 +1,97 @@
+import random
+import string
+from termcolor import colored
+from colorama import Fore, Style
+text = """ ⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢸⣿⡿⢿⡟⠩⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠸⣏⠀⠋⠡⠔⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣾⣟⡀⠀⠀⠆⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀
+⠀⠀⠀⠀⣠⣼⣿⣿⣼⠢⠁⠀⠀⠀⠀⠀⢀⣾⣿⣿⣦⡀
+⠀⢠⣾⣿⣾⣿⣿⣿⣿⣿⣾⣷⡆⠀⠀⠀⢾⣿⣏⣩⣿⡇
+⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣄⠀⠀⠘⢿⣿⣿⣿⠇
+⢀⣾⡿⡫⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⡆⣠⣾⡯⠉⠉⠁⠀
+⢸⣿⣿⣷⣽⣿⣿⣽⣿⣿⣶⡾⣹⣷⣯⣿⡿⠁⠀⠀⠀⠀
+⢸⣿⣿⣿⠹⣿⣿⣿⣿⣾⣿⡇⠙⢿⡿⡽⠁⠀⠀⠀⠀⠀
+⣤⣾⡙⠉⢻⣿⣿⣯⣿⣤⡿⡅⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀
+⣿⣿⡆⠀⢸⣿⣿⢯⣿⣿⣷⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢹⣿⣧⠀⣰⣿⣿⣾⣿⢿⠟⢡⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠸⢿⠹⠀⠉⠉⠋⠉⠉⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢻⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+This is a script to update your method
+ and components And every day a new update is made for him🖐
+ """
+colored_text = colored(text, 'red')
+print(colored_text)
+
+options = [
+    "Report.py/5.1.0.6.8.1.5.1.server.v.7a.8.6.1.0×0.0.virus.link.2.8.2.9.1.0.1.7.2.3.7.6.9.RaKs+0000.4/¥fttks-999999999_NULL_1223334_filterint.com",
+    "BuG-supportbot.ir/#c=b0Y0a2cafbaf668e282d2dc02a1fe2a7(/*filterig-account*/)Error-404.filter.5.106.8.151.ip.server.v7a/filter.xml",
+    "http://www.linux.org/[Fil=user.hacker]1.0.4.2.6.1.4.7.2.terminal.0.0.5.4.termux.34.217.203.150.hacker.Web.Phishing.virus.in.rubika/Cyberpolice.gov.ir", "https://Fyl-ter.rubika.xml/(+http://wbz2lrxhw4dd7h5t2wnoczmcz5snjpym4pr7dzjmah4vi6yywn37bdyd.onion/+)filter.user.hacker.viru3.Phishing.5.160.10.200/reset_99977_hacked.terminal(12233-4)", "Report.md/4.0.7.5.filtering.rubika=b0Y0a2cafbaf668e282d2dc02a1fe2a7/reset_22.24.93/#bug-SupportBot.1.4.2.6.8.fil.user.5.1.6.0.1.0.2.0.0.netreport", "pornpics.com/yftt+18k.2.1.3.1.7.4.1.4.sex.chat.9.18.0.1.4.2.6.8(https://bitly.ws/332gs)7.8.9.2.5.6.8.3.Xct/Spam.account/5.106.8.151.SupportBot", "spamhaus.org/3.6.1.8.3.6.1.6.2.6.2.7/A片/server.7a.3.5.1/filtering_account/6.1.6.1.7.2.6.1.CT.5.1.6.1.8.3.5.1.4.1.6.1.5.1.6"
+, "https://report.html/1.4.1.4//yftt+18k//2.4.1.5.7.5.1.5.ip.server=[5.106.8.151]/fil.fil.fil.4.6.1.6.6.1.6.9.2.7.7/大象/4.5.1.7.1.5.1.5.6.6.性別.4.6.1.5.8.1.4.DarkweB.3.3.5.6.7.1.6.8.2.6.7.1.5.6.2.5.6.4.7.1.5.7.1.魔術方塊.8.3.1.4.4.1.5.4.5.5.1.5.6.6.1.supportbot/filtering.account.com"]
+
+def main():
+    print("1: link sexy")
+    print("2: site sex")
+    print("3: ip site sex")
+    print("4: github sexy")
+    print("5: link hack")
+    print("6: link phishing")
+    print("7: link virus")
+    print("8: mokhareb php")
+    print("9: mokhareb darkweb")
+    print("10: mokhareb fata")
+    print("11: site teror")
+    print("12: site anti islam")
+    print("13: site anti leadership")
+    print("14: bug porn")
+    print("15: ip rubika")
+    print("16: bug server")
+    print("17: bug termux")
+    print("18: bug linux")
+    print("19: Algorithm")
+
+    choice = input("Choose an option 👉 ")
+
+    if choice == '1':
+        print(Fore.GREEN + "https://uploadkon.ir/uploads/498f27_24-video-al6f.jpg")
+    elif choice == '2':
+        print(Fore.GREEN + "https://m.me11r.com/tags/sex/")
+    elif choice == '3':
+    	print(Fore.GREEN + "https://185.73.220.197")
+    elif choice == '4':
+        print(Fore.GREEN + "https://github.com/xneon2/NAUGHTY-HATZU")
+    elif choice == '5':
+        print(Fore.GREEN + "https://www.hacker101.com/playlists/web_hacking.html")
+    elif choice == '6':
+        print(Fore.GREEN + "https://www.com/Phishingport")
+    elif choice == '7':
+        print(Fore.GREEN + "https://github.com/java-virus")
+    elif choice == '8':
+        print(Fore.GREEN + "http://support.clean-mx.de/clean-mx/viruses.php")
+    elif choice == '9':
+        print(Fore.GREEN + "https://darkweb-sites.org/the-hidden-internet-a-dark-website-directory-handbook/")
+    elif choice == '10':
+        print(Fore.GREEN + "https://cyberpolice.gov.ir/node/168853")
+    elif choice == '11':
+        print(Fore.GREEN + "https://websitehomedaesh.blog.ir")
+    elif choice == '12':
+        print(Fore.GREEN + "https://www.merriam-webster.com/dictionary/anti-Islamic")
+    elif choice == '13':
+        print(Fore.GREEN + "https://theintercept.com/2022/12/09/iran-regime-khamenei-death")
+    elif choice == '14':
+        print(Fore.GREEN + "https://github.com/huvakhashtrah/bug-rubika-report-huvakhashtrah/blob/main/Bug.porn.in.rubika.io")
+    elif choice == '15':
+        print("5.106.8.151")
+    elif choice == '16':
+        print(Fore.GREEN + "http://Server.onlline/server.offline.onlline")
+    elif choice == '17':
+        print(Fore.GREEN + "termux.dev/Spam%100/bug-0.0-mirro")
+    elif choice == '18':
+        print(Fore.GREEN + "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.zip")
+    elif choice == '19':
+    	print(Fore.GREEN + random.choice(options))
+    else:
+        print("Invalid choice! Please enter a valid option.")
+
+main()
